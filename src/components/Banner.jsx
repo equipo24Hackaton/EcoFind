3f8930c22d1ecce09338f389f462b21a5cd32f01
silvio-aguilar <silvioaguilartorres@outlook.com>
2023-07-19 15:40:12 +0200

    Home

---
+++ src/components/Banner.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Banner = () =>  {
+  return (
+    <div className='containerBanner'>
+        <div className="banner">
+        
+        <div className='imgBanner'>
+        <img  src="ruta-del-logo.png" alt="Banner" />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+
+export default Banner;
---
+++ src/components/Categoria.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+const Categoria = () => {
+  return (
+    <div className='categoria'></div>
+  )
+}
+
+export default Categoria;
