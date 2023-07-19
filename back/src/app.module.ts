5e11b0d7b5b787af16e7a41bb9a0f726569f407e
Angela-GM <angelagm0112@gmail.com>
2023-07-19 17:08:14 +0200

    connect to mongodb

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,12 +1,25 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { UserModule } from './user/user.module';
+import { UserController } from './user/user.controller';
+import { ProductsController } from './products/products.controller';
+import { UserService } from './user/user.service';
+import { ProductsService } from './products/products.service';
+import { MongooseModule } from '@nestjs/mongoose'; // Agregar esta importación
+import { ConfigModule } from '@nestjs/config'; // Agregar esta importación
 
 @Module({
-  imports: [ProductsModule, UserModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    // STATIC CONNECTION VERSION
+    MongooseModule.forRoot('mongodb://127.0.0.1:27017/ecofind'),
+
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    UserModule,
+    ProductsModule,
+  ],
+  controllers: [UserController, ProductsController],
+  providers: [UserService, ProductsService],
 })
 export class AppModule {}
