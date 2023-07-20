e1efd6416085da18a47dc19166a5bb5cf2a4949d
Angela-GM <angelagm0112@gmail.com>
2023-07-20 19:03:02 +0200

    crear buscador

--- front/src/services/ApiConection.jsx
+++ front/src/services/ApiConection.jsx
@@ -1,14 +1,16 @@
-// apiService.js
-
 import axios from 'axios';
 
 const apiService = axios.create({
   baseURL: 'http://localhost:8000',
 });
 
-export const fetchProducts = async () => {
+export const fetchProducts = async (searchQuery) => { // Agrega el parámetro searchQuery
   try {
-    const response = await apiService.get('/products');
+    const response = await apiService.get(
+      searchQuery === ""
+        ? "/products"
+        : `/products/search/${searchQuery}`
+    );
     return response.data;
   } catch (error) {
     throw new Error('Error fetching products');
